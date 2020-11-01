ff05484395b303a3e7bcaa93f03552bcb88893ce
hrandrianasolo <hjsrandrianasolo@gmail.com>
2020-11-01 22:02:17 +0100

    Add return button and functionality to print article below threshold

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from blog.models import Provider, Article
+from django.template.context_processors import request
 
 
 def home(request):
@@ -16,6 +17,13 @@ def provider(request):
 def article(request):
     list_article = Article.objects.all().order_by('-name')
     return render(request, 'blog/article.html', {'listarticle': list_article})
+
+
+def article_below_threshold(request):
+    list_article = Article.objects.all()
+    list_article = [x for x in list_article if x.seuil >= x.stock ]
+    return render(request, 'blog/article_below_threshold.html', {'listarticle': list_article})
+
 
 
 def edit_provider(request, provide_id):
@@ -36,6 +44,49 @@ def edit_provider(request, provide_id):
         return render(request, 'blog/edit_provider.html', {'provider': current_provider})
 
 
+def new_provider(request):
+    current_provider = Provider()
+    if request.POST:
+        if request.POST.get('name') != "":
+            current_provider.name = request.POST.get('name')
+        if request.POST.get('address') != "":
+            current_provider.address = request.POST.get('address')
+        if request.POST.get('zip_code') != "":
+            current_provider.zip_code = request.POST.get('zip_code')
+        if request.POST.get('phone') != "":
+            current_provider.phone = request.POST.get('phone')
+
+        current_provider.save()
+        return HttpResponseRedirect("/provider")
+    else:
+        return render(request, 'blog/new_provider.html', {'provider': current_provider})
+
+
+def new_article(request):
+    current_article = Article()
+    list_provider = Provider.objects.all().order_by('-name')
+    if request.POST:
+        if request.POST.get('name') != "":
+            current_article.name = request.POST.get('name')
+        if request.POST.get('price') != "":
+            current_article.price = request.POST.get('price')
+        if request.POST.get('barcode') != "":
+            current_article.barcode = request.POST.get('barcode')
+        if request.POST.get('stock') != "":
+            current_article.stock = request.POST.get('stock')
+        if request.POST.get('seuil') != "":
+            current_article.seuil = request.POST.get('seuil')
+        if request.POST.get('is_sold_with_weight') != "":
+            current_article.is_sold_with_weight = bool(request.POST.get('is_sold_with_weight'))
+        if request.POST.get('provider') != "":
+            current_article.provider = Provider.objects.get(id=request.POST.get('provider'))
+
+        current_article.save()
+        return HttpResponseRedirect("/article")
+    else:
+        return render(request, 'blog/new_article.html', {'article': current_article, 'list_provider': list_provider})
+
+
 def edit_article(request, article_id):
     current_article = Article.objects.get(id=article_id)
     list_provider = Provider.objects.all().order_by('-name')
@@ -51,7 +102,7 @@ def edit_article(request, article_id):
         if request.POST.get('seuil') != "":
             current_article.seuil = request.POST.get('seuil')
         if request.POST.get('is_sold_with_weight') != "":
-            current_article.is_sold_with_weight = request.POST.get('is_sold_with_weight')
+            current_article.is_sold_with_weight = bool(request.POST.get('is_sold_with_weight'))
         if request.POST.get('provider') != "":
             current_article.provider = Provider.objects.get(id=request.POST.get('provider'))
 
